75544c72ca362112befc4cea6d26744960149248
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:10:20 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        users: []
+    }
+    componentDidMount() {
+       const users = [
+            {
+                name: 'Faxriyor',
+                age: 21,
+                job: 'web developer'
+
+            },
+            {
+                name: 'Oysanam',
+                age: 20,
+                job: 'tarjimon'
+
+            },
+            {
+                name: 'Farrux',
+                age: 21,
+                job: 'bekorchi'
+
+            },
+            {
+                name: 'palancha',
+                age: 213,
+                job: 'toza havoga xujayin'
+
+            }
+        ]
+        this.setState({
+            users: users
+        })
+    }
+
+    onClickRemove = (index) => {
+        this.state.users.splice(index, 1)
+        this.setState({
+            users: this.state.users
+        })
+    }
+    render() {
+        const {users} = this.state
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="table">
+                            <thead>
+                                <tr>
+                                    <th>N</th>
+                                    <th>Name</th>
+                                    <th>Age</th>
+                                    <th>Job</th>
+                                    <th>remove</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users.map((type, index) => 
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{type.name}</td>
+                                            <td>{type.age}</td>
+                                            <td>{type.job}</td>
+                                            <td><button className="btn btn-danger" onClick={() => this.onClickRemove(index)}>remove</button></td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
